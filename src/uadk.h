35a40c27d1e6a6f29aa7949bd1857b9299e3208c
sunhaitao123 <54341208+sunhaitao123@users.noreply.github.com>
2021-12-30 11:48:08 +0800

    change

--- src/uadk.h
+++ src/uadk.h
@@ -35,7 +35,7 @@ typedef struct _NID_map {
 
 
 extern const char *engine_uadk_id;
-extern void uadk_e_destory_aead(NID_map *info, int num);
+extern void uadk_e_destroy_aead(NID_map *info, int num);
 extern void uadk_e_destroy_cipher(NID_map *info, int num);
 extern int uadk_e_bind_digest(ENGINE *e);
 extern void uadk_e_destroy_digest(void);
