e5df45a2454ee40b50ebd91786204d8553652699
sht <91588238@qq.com>
2021-12-30 10:42:46 +0800

    add aes-gcm

--- src/e_uadk.c
+++ src/e_uadk.c
@@ -19,19 +19,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <dlfcn.h>
-#include <openssl/engine.h>
 #include <uadk/wd.h>
 #include "uadk.h"
 #include "uadk_async.h"
+#include "uadk_cipher.h"
+#include "uadk_aead.h"
 #ifdef KAE
 #include "v1/uadk_v1.h"
 #endif
 
 #define UADK_CMD_ENABLE_CIPHER_ENV	ENGINE_CMD_BASE
-#define UADK_CMD_ENABLE_DIGEST_ENV	(ENGINE_CMD_BASE + 1)
-#define UADK_CMD_ENABLE_RSA_ENV		(ENGINE_CMD_BASE + 2)
-#define UADK_CMD_ENABLE_DH_ENV		(ENGINE_CMD_BASE + 3)
-#define UADK_CMD_ENABLE_ECC_ENV		(ENGINE_CMD_BASE + 4)
+#define UADK_CMD_ENABLE_AEAD_ENV	(ENGINE_CMD_BASE + 1)
+#define UADK_CMD_ENABLE_DIGEST_ENV	(ENGINE_CMD_BASE + 2)
+#define UADK_CMD_ENABLE_RSA_ENV		(ENGINE_CMD_BASE + 3)
+#define UADK_CMD_ENABLE_DH_ENV		(ENGINE_CMD_BASE + 4)
+#define UADK_CMD_ENABLE_ECC_ENV		(ENGINE_CMD_BASE + 5)
 
 /* Constants used when creating the ENGINE */
 const char *engine_uadk_id = "uadk_engine";
@@ -56,6 +58,12 @@ static const ENGINE_CMD_DEFN g_uadk_cmd_defns[] = {
 		"UADK_CMD_ENABLE_CIPHER_ENV",
 		"Enable or Disable cipher engine environment variable.",
 		ENGINE_CMD_FLAG_NUMERIC
+	},
+	{
+		UADK_CMD_ENABLE_AEAD_ENV,
+                "UADK_CMD_ENABLE_AEAD_ENV",
+                "Enable or Disable digest engine environment variable.",
+                ENGINE_CMD_FLAG_NUMERIC
 	},
 	{
 		UADK_CMD_ENABLE_DIGEST_ENV,
@@ -103,11 +111,92 @@ struct uadk_alg_env_enabled {
 
 static struct uadk_alg_env_enabled uadk_env_enabled[] = {
 	{ "cipher", 0 },
+	{ "aead",   0 },
 	{ "digest", 0 },
 	{ "rsa", 0 },
 	{ "dh", 0 },
 	{ "ecc", 0 }
 };
+
+int uadk_cipher_nids_920[] = {
+        NID_aes_128_cbc,
+        NID_aes_192_cbc,
+        NID_aes_256_cbc,
+        NID_aes_128_ecb,
+        NID_aes_192_ecb,
+        NID_aes_256_ecb,
+        NID_aes_128_xts,
+        NID_aes_256_xts,
+        NID_sm4_cbc,
+        NID_des_ede3_cbc,
+        NID_des_ede3_cbc,
+        NID_aes_128_gcm,
+        NID_aes_192_gcm,
+        NID_aes_256_gcm,
+};
+
+int uadk_cipher_nids_930[] = {
+        NID_aes_128_cbc,
+        NID_aes_192_cbc,
+        NID_aes_256_cbc,
+        NID_aes_128_ctr,
+        NID_aes_192_ctr,
+        NID_aes_256_ctr,
+        NID_aes_128_ecb,
+        NID_aes_192_ecb,
+        NID_aes_256_ecb,
+        NID_aes_128_xts,
+        NID_aes_256_xts,
+        NID_sm4_cbc,
+        NID_sm4_ebc,
+        NID_des_ede3_cbc,
+        NID_aes_ede3_ecb,
+        NID_aes_128_cfb128,
+        NID_aes_192_cfb128,
+        NID_aes_256_cfb128,
+        NID_aes_128_ofb128,
+        NID_aes_192_ofb128,
+        NID_aes_256_ofb128,
+        NID_sm4_cfb128,
+        NID_sm4_ofb128,
+        NID_sm4_ctr,
+        NID_aes_128_gcm,
+        NID_aes_192_gcm,
+        NID_aes_256_gcm,       
+};
+
+static NID_map info[] = {
+        {NID_aes_128_cbc, NULL}
+        {NID_aes_192_cbc, NULL}
+        {NID_aes_256_cbc, NULL}
+        {NID_aes_128_ctr, NULL}
+        {NID_aes_192_ctr, NULL}
+        {NID_aes_256_ctr, NULL}
+        {NID_aes_128_ecb, NULL}
+        {NID_aes_192_ecb, NULL}
+        {NID_aes_256_ecb, NULL}
+        {NID_aes_128_xts, NULL}
+        {NID_aes_256_xts, NULL}
+        {NID_sm4_cbc, NULL}
+        {NID_sm4_ebc, NULL}
+        {NID_des_ede3_cbc, NULL}
+        {NID_aes_ede3_ecb, NULL}
+        {NID_aes_128_cfb128, NULL}
+        {NID_aes_192_cfb128, NULL}
+        {NID_aes_256_cfb128, NULL}
+        {NID_aes_128_ofb128, NULL}
+        {NID_aes_192_ofb128, NULL}
+        {NID_aes_256_ofb128, NULL}
+        {NID_sm4_cfb128, NULL}
+        {NID_sm4_ofb128, NULL}
+        {NID_sm4_ctr, NULL}
+        {NID_aes_128_gcm, NULL}
+        {NID_aes_192_gcm, NULL}
+        {NID_aes_256_gcm, NULL}
+};
+
+static const unsigned int num_cc = (sizeof(info) / sizeof(info[0])); 
+
 
 int uadk_e_is_env_enabled(const char *alg_name)
 {
@@ -175,6 +264,9 @@ static int uadk_engine_ctrl(ENGINE *e, int cmd, long i,
 	case UADK_CMD_ENABLE_CIPHER_ENV:
 		uadk_e_set_env_enabled("cipher", i);
 		break;
+	case UADK_CMD_ENABLE_AEAD_ENV:
+		uadk_e_set_env_enabled("aead", i);
+		break;
 	case UADK_CMD_ENABLE_DIGEST_ENV:
 		uadk_e_set_env_enabled("digest", i);
 		break;
@@ -207,8 +299,10 @@ static int uadk_destroy(ENGINE *e)
 		hpre_dh_destroy();
 #endif
 
-	if (uadk_cipher)
-		uadk_e_destroy_cipher();
+        if (uadk_cipher) {
+                uadk_e_destroy_cipher(info, num_cc);
+                uadk_e_destroy_aead(info, num_cc);
+        }
 	if (uadk_digest)
 		uadk_e_destroy_digest();
 	if (uadk_rsa)
@@ -291,13 +385,92 @@ static void bind_fn_kae_alg(ENGINE *e)
 }
 #endif
 
+void uadk_e_create_cipher(int idx)
+{
+        switch(info[idx].nid) {
+                case NID_aes_128_gcm;
+                case NID_aes_192_gcm;
+                case NID_aes_256_gcm;
+                        info[idx].cipher = (EVP_CIPHER *)uadk_create_gcm_cipher_meth(info[idx].nid);
+                break;
+                case NID_aes_128_cbc;
+            case NID_aes_192_cbc;
+            case NID_aes_256_cbc;
+            case NID_aes_128_ctr;
+            case NID_aes_192_ctr;
+            case NID_aes_256_ctr;
+            case NID_aes_128_ecb;
+            case NID_aes_192_ecb;
+            case NID_aes_256_ecb;
+            case NID_aes_128_xts;
+            case NID_aes_256_xts;
+            case NID_sm4_cbc;
+            case NID_sm4_ecb;
+            case NID_des_ede3_cbc;
+            case NID_des_ede3_ecb;
+            case NID_aes_128_cfb128;
+            case NID_aes_192_cfb128;
+            case NID_aes_256_cfb128;
+            case NID_aes_128_ofb128;
+            case NID_aes_192_ofb128;
+            case NID_aes_256_ofb128;
+            case NID_sm4_cfb128;
+            case NID_sm4_ofb128;
+            case NID_sm4_ctr;
+                        info[index].cipher = (EVP_CIPHER *)uadk_create_cipher_meth(info[index].nid, info[index].keylen, platform);
+                break;
+            default:
+                       break;
+       }
+}
+
+static int uadk_e_ciphers(ENGINE *e, const EVP_CIPHER **cipher, const int **nids, int nid)
+{
+        int i, platform;
+        struct uacce_dev *dev;
+        if(unlikely((nids == NULL) && ((cipher == NULL) || (nid < 0)))) {
+                if(cipher != NULL)
+                        *cipher = NULL;
+                return 0;
+        }
+
+        dev = wd_get_accel_dev("cipher");
+        if (!strcmp(dev->api, "hisi_qm_v2"))
+                        platform = KUNPENG920;
+                else
+                        platform = KUNPENG930;
+
+        if (cipher == NULL) {
+                 if (platform == KUNPENG920) {
+                         *nids = uadk_cipher_nids_920;
+                         return (sizeof(uadk_cipher_nids_920) / sizeof(uadk_cipher_nids_920[0]));
+                 } else {
+                         *nids = uadk_cipher_nids_930;
+                         return (sizeof(uadk_cipher_nids_930) / sizeof(uadk_cipher_nids_930[0]));
+                 }
+        }
+
+        for(i = 0; i < num_cc; i++) {
+                if(nid == info[i].nid) {
+                        if(info[i].cipher == NULL)
+                        {
+                                uadk_e_create_ciphers(i);
+                        }
+                        *cipher = info[i].cipher;
+                        return 1;
+                }
+        }
+        *cipher = NULL;
+        return 0;
+}
+
 static void bind_fn_uadk_alg(ENGINE *e)
 {
 	struct uacce_dev *dev;
 
 	dev = wd_get_accel_dev("cipher");
-	if (dev) {
-		if (!uadk_e_bind_cipher(e))
+	if(dev) {
+		if (!ENGINE_set_ciphers(e, uadk_e_ciphers))
 			fprintf(stderr, "uadk bind cipher failed\n");
 		else
 			uadk_cipher = 1;
---
+++ src/uadk_aead.h
@@ -0,0 +1,3 @@
+#include <openssl/engine.h>
+
+const EVP_CIPHER *uadk_create_gcm_cipher_meth(int nid);
